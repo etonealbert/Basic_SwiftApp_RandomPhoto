e3fa94b52b6413a0602a8e4a395818c104db4bcd
etonealbert <albert.lukmanov99@gmail.com>
2022-04-14 19:08:24 +0300

    first commit

---
+++ RandomPhoto/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  RandomPhoto
+//
+//  Created by Albert on 10.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        view.backgroundColor = .systemPink
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomPhoto/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  RandomPhoto
+//
+//  Created by Albert on 10.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
